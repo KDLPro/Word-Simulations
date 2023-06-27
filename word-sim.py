59405d13bf7e19a9ac4b59e78118a5117626c5c1
KDLPro <38602758+KDLPro@users.noreply.github.com>
2023-06-28 00:11:08 +0800

    Modified RNG letter generation function names

--- word-sim.py
+++ word-sim.py
@@ -25,13 +25,16 @@ class wordle_sim:
     def setClosestWord(self, word):
         self.closest_word = word
 
-    def randomize_letter(self):
+    def rando_letter(self):
         index = random.randint(1, 26)
         letter = chr(index + 96)
         return letter
     
-    def randomize_letter_v2(self):
-        # Chances taken from: https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+    def rando_letter_v2(self):
+        """ 
+        Chances taken from: https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+        The higher the chance, the more likely for the letter to get picked.
+        """
         letters = "abcdefghijklmnopqrstuvwxyz"
         letters_freq = [0.08167, 0.09659, 0.12441, 0.16694, 0.29396, 0.31624, 0.33639, 0.39733, 
                         0.46699, 0.46852, 0.47624, 0.51649, 0.54055, 0.60804, 0.68311, 0.70240,
@@ -41,15 +44,18 @@ class wordle_sim:
         rng = round(rng, 5)
         
         for i in range(26):
-            if rng == letters_freq[i]:
+            if rng < letters_freq[i]:
                 return letters[i]
 
-    def randomize_vowels(self):
+    def rando_vowel(self):
         vowels = "aeiou"
         return random.choice(vowels)
     
-    def randomize_vowels_v2(self):
-        # Chances taken from: https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+    def rando_vowel_v2(self):
+        """ 
+        Chances taken from: https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+        The higher the chance, the more likely for the vowel to get picked.
+        """
         vowels = "aeiou"
         vowel_freq = [0.21436, 0.54774, 0.730578, 0.92761, 1]
         rng = random.random()
@@ -59,22 +65,32 @@ class wordle_sim:
             if rng < vowel_freq[i]:
                 return vowels[i]
 
-    def randomize_consonants(self):
+    def rando_consonant(self):
         consonants = "bcdfghjklmnpqrstvwxyz"
         return random.choice(consonants)
     
-    def randomize_consonants_v2(self):
-        # Chances taken from: https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+    def rando_plural_consonant(self):
+        # Generates consonants that can only be turned to plural by adding '-es'.
+        consonants = "sxz"
+        return random.choice(consonants)
+    
+    def rando_consonant_v2(self):
+        """ 
+        Chances taken from: https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+        The higher the chance, the more likely for the consonant to get picked.
+        """
         consonants = "bcdfghjklmnpqrstvwxyz"
         cons_freq = [0.02410, 0.06905, 0.13776, 0.17375, 0.20630, 0.30475, 0.30723, 0.31670,
                     0.38472, 0.42359, 0.53263, 0.56379, 0.56532, 0.66205, 0.76426, 0.91056,
                     0.94869, 0.95111, 0.98300, 1]
         
-        
         rng = random.random()
         rng = round(rng, 5)
-        
-        for i in range(5):
+
+        for i in range(21):
+            if rng < cons_freq[i]:
+                return consonants[i]
+            
             if rng < cons_freq[i]:
                 return consonants[i]
     
@@ -96,12 +112,14 @@ class wordle_sim:
         guess = self.replace_char_at_index(mid_vowel, guess, position)
         
         mid_vowel = self.randomize_vowels()
+        mid_vowel = self.rando_vowel()
         rng_index = random.randint(0, 1)
         empty_letters.remove((mid_position[rng_index]))
         guess = self.replace_char_at_index(mid_vowel, guess, mid_position[rng_index])
         
         for i in empty_letters:
             consonant = self.randomize_consonants()
+            consonant = self.rando_consonant()
             guess = self.replace_char_at_index(consonant, guess, i)
 
         print("Guessed word: " + guess)
@@ -118,19 +136,19 @@ class wordle_sim:
         empty_letters = [0, 1, 2, 3, 4]
         mid_position = [1, 2, 3]
 
-        mid_vowel = self.randomize_vowels_v2()
+        mid_vowel = self.rando_vowel_v2()
         position = random.randint(1, 3)
         empty_letters.remove(position)
         mid_position.remove(position)
         guess = self.replace_char_at_index(mid_vowel, guess, position)
         
-        mid_vowel = self.randomize_vowels_v2()
+        mid_vowel = self.rando_vowel_v2()
         rng_index = random.randint(0, 1)
         empty_letters.remove((mid_position[rng_index]))
         guess = self.replace_char_at_index(mid_vowel, guess, mid_position[rng_index])
         
         for i in empty_letters:
-            consonant = self.randomize_consonants_v2()
+            consonant = self.rando_consonant_v2()
             guess = self.replace_char_at_index(consonant, guess, i)
 
         print("Guessed word: " + guess)
@@ -151,7 +169,7 @@ class wordle_sim:
         guess = self.replace_char_at_index(mid_vowel, guess, position)
         
         for i in empty_letters:
-            consonant = self.randomize_consonants()
+            consonant = self.rando_consonant_v2()
             guess = self.replace_char_at_index(consonant, guess, i)
 
         print("Guessed word: " + guess)
